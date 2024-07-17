ebbb04820b3507eda5cbd2fafa3dbd2ee5b36a18
Vichini Vimarshani <iit19055@std.uwu.ac.lk>
2024-07-17 12:21:25 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,11 @@ import { AppModule } from './app.module';
 const express = require("express");
 import bodyParser from "body-parser";
 const cors = require("cors");
-
+import * as process from "process"
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
  
   app.use(cors());
-  await app.listen(8090);
+  await app.listen(process.env.PORT,'0.0.0.0');
 }
 bootstrap();
